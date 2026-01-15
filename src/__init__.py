a17cdf9c8317a4ca11b24bbcae706e0d22f9a95c
TISEPSE <titi21player@gmail.com>
2026-01-15 12:20:55 +0100

    modif

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import os
 
 from src.forms import forms_blueprint
 from src.auth import auth_blueprint
 
 
 def create_app():
-    app = Flask(__name__, template_folder='templates', static_folder='static')
+    app = Flask(__name__,
+                template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'),
+                static_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static'))
     app.register_blueprint(forms_blueprint)
     app.register_blueprint(auth_blueprint)
     return app
